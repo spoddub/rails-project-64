6517472afb6f1982a5d0fcaaaf8cdd4f665336fe
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-19 16:37:34 +0300

    add health check

---
+++ test/healthcheck_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class HealthcheckTest < ActionDispatch::IntegrationTest
+  test "GET /up returns 200" do
+    get "/up"
+    assert_response :success
+    assert { response.status == 200 }
+  end
+
+  test "faker generates an email-looking string" do
+    email = Faker::Internet.email
+    assert { email.match?(/\A.+@.+\..+\z/) }
+  end
+end
