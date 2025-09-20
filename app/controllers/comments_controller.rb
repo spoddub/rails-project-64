d11f8e776b707a1f66b399f594be3ea1b5395269
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-21 02:09:35 +0300

    fix comment ids

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,20 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.post_comments.build(comment_params.merge(user: current_user))
 
-    if params[:parent_id].present?
-      parent = @post.post_comments.find_by(id: params[:parent_id])
-      @comment.parent = parent if parent
-    end
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.post_comments.build(comment_params.merge(user: current_user))
+
     if @comment.save
-      redirect_to post_path(@post, anchor: dom_id(@comment)), notice: "Комментарий добавлен"
+      redirect_to post_path(post), notice: "Комментарий добавлен"
     else
-      @comment.errors.add(:base, "Не удалось сохранить комментарий") if @comment.errors.empty?
-      render "posts/show", status: :unprocessable_entity
+      redirect_to post_path(post), alert: @comment.errors.full_messages.to_sentence
     end
   end
 
   private
 
   def comment_params
-    params.require(:post_comments).permit(:content)
+    params.require(:post_comment).permit(:content, :parent_id)
   end
 end
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,6 +1,8 @@
 class PostComment < ApplicationRecord
   has_ancestry ancestry_format: :materialized_path2
-  belongs_to :user
+
   belongs_to :post
+  belongs_to :user
+
   validates :content, presence: true
 end
