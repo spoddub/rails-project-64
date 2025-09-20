fe9f13769325836aeec498a274731bfa032d896f
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-20 16:42:13 +0300

    add routing for acconunt and posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # Healthcheck
   get "up" => "rails/health#show", as: :rails_health_check
+  namespace :account do
+    resources :posts, only: %i[index new create]
+  end
 end
