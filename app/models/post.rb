e8e88f579e868f67bfae3915e9581a576e1f80a5
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-21 16:30:34 +0300

    fix post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,10 @@ class Post < ApplicationRecord
   belongs_to :creator, class_name: "User"
 
   has_many :post_comments, dependent: :destroy
+  has_many :comments, class_name: "PostComment", dependent: :destroy
 
   has_many :post_likes, dependent: :destroy
   has_many :likers, through: :post_likes, source: :user
-
-  # for CI tests:
   has_many :likes, class_name: "PostLike", dependent: :destroy
 
   validates :title, :body, presence: true
