44fc3f94240e3f28bb4a5937fc7bed4cade91900
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-22 03:13:02 +0300

    remove explicit end statements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def safe_l(time, format: :short, **opts)
+    return '' if time.nil?
+
+    l(time, { format: format }.merge(opts))
+  rescue StandardError
+    time.to_s
+  end
 end
